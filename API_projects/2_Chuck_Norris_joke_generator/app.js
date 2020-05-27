3c8f00ddeb9a50009c197fa34a0ef6da21002482
kostyrko <mkostyrko@gmail.com>
2020-05-27 21:03:22 +0200

    Initial commit

---
+++ API_projects/2_Chuck_Norris_joke_generator/app.js
@@ -0,0 +1,20 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value;
+
+  const xhr = new XMLHttpRequest();
+
+  // true -> at the end to be asynchronous
+  xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = this.responseText;
+      console.log(response)
+    }
+  }
+
+  xhr.send();
+  e.preventDefault();
+}
