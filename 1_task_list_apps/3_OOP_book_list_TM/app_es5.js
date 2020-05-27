13bf1513dbd87887173d6bc1886cfdd17bca139e
kostyrko <mkostyrko@gmail.com>
2020-05-27 16:12:04 +0200

    Add delete method

--- 1_task_list_apps/3_OOP_book_list_TM/app_es5.js
+++ 1_task_list_apps/3_OOP_book_list_TM/app_es5.js
@@ -49,6 +49,14 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 }
 
+// Delete book
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
+// Event listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // when submit create variables with input values
   const title = document.getElementById('title').value,
@@ -76,4 +84,19 @@ document.getElementById('book-form').addEventListener('submit', function(e){
   }
 
   e.preventDefault();
-})
+});
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener(
+  'click', function(e) {
+
+    const ui = new UI();
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert('Book removed', 'success')
+
+    e.preventDefault();
+  }
+)
+
