ccf9b445efe94d35eeb57f3e69fa1a4ea681fb48
kostyrko <mkostyrko@gmail.com>
2020-05-28 14:04:24 +0200

    Add more data

--- 2_API_apps/3_SWAPI/app.js
+++ 2_API_apps/3_SWAPI/app.js
@@ -1,19 +1,26 @@
 // https://swapi.dev/
 // https://swapi.dev/documentation
 
-document.querySelector('button').addEventListener('click', findSomeone);
-const name = document.querySelector('.name')
+document.querySelector("button").addEventListener("click", findSomeone);
+const name    = document.querySelector(".name");
+const height  = document.querySelector(".height");
+const mass    = document.querySelector(".mass");
+const birth   = document.querySelector(".birth");
 
-function findSomeone () {
+function findSomeone() {
   // randomize 88 stands for number of people in API
-  let randomNum = Math.floor((Math.random()*88) +1);
-
+  let randomNum = Math.floor(Math.random() * 88 + 1);
   // add then as the request takes time = get and after you get it (then) use response.data etc
-  axios.get(`https://swapi.dev/api/people/${randomNum}`).then(function(response){
-    updateInfo(response.data);
-  })
+  axios
+    .get(`https://swapi.dev/api/people/${randomNum}`)
+    .then(function (response) {
+      updateInfo(response.data);
+    });
 }
 
 function updateInfo(data) {
-  name.innerText = data.name
+  name.innerText = data.name;
+  height.innerText = `Height: ${data.height}`;
+  mass.innerText = `Mass: ${data.mass}`;
+  birth.innerText = `Birth year: ${data.birth_year}`;
 }
