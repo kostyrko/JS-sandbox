4dc6c6a6438676cc6e7e7ac411177901e3a6badd
kostyrko <mkostyrko@gmail.com>
2020-05-28 13:56:00 +0200

    Add find random person

--- 2_API_apps/3_SWAPI/app.js
+++ 2_API_apps/3_SWAPI/app.js
@@ -2,9 +2,18 @@
 // https://swapi.dev/documentation
 
 document.querySelector('button').addEventListener('click', findSomeone);
+const name = document.querySelector('.name')
 
 function findSomeone () {
-  axios.get('https://swapi.dev/api/people/1').then(function(response){
-    console.log(response.data);
+  // randomize 88 stands for number of people in API
+  let randomNum = Math.floor((Math.random()*88) +1);
+
+  // add then as the request takes time = get and after you get it (then) use response.data etc
+  axios.get(`https://swapi.dev/api/people/${randomNum}`).then(function(response){
+    updateInfo(response.data);
   })
+}
+
+function updateInfo(data) {
+  name.innerText = data.name
 }
