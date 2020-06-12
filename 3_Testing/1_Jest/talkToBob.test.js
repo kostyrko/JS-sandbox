55654ee14887af2a8c610af447c5b00e217734da
kostyrko <mkostyrko@gmail.com>
2020-06-12 13:00:32 +0200

    Add test 3-5

--- 3_Testing/1_Jest/talkToBob.test.js
+++ 3_Testing/1_Jest/talkToBob.test.js
@@ -6,6 +6,15 @@ describe('Function - talkToBob', () => {
     expect(talkToBob("How are You?")).toBe("Sure")
   });
   test("Answers 'Whoa, chill out!' if you yell at him", () => {
-    expect(talkToBob("WHAT?")).toBe("Whoa, chill out!")
+    expect(talkToBob("DON'T")).toBe("Whoa, chill out!")
+  });
+  test("He retorts 'Calm down, I know what I'm doing!' if you yell a question at him.", () => {
+    expect(talkToBob("WHAT?")).toBe("Calm down, I know what I'm doing!")
+  });
+  test("He says 'Fine. Be that way!' if you address him without actually saying anything.", () => {
+    expect(talkToBob("")).toBe('Fine. Be that way!')
+  });
+  test("He answers 'Whatever.' to anything else.", () => {
+    expect(talkToBob("whatever")).toBe('Whatever.')
   });
 });
