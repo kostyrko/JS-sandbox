1186369ba7fcd134568d3aa13e0144495490d6c3
kostyrko <mkostyrko@gmail.com>
2020-06-12 13:01:19 +0200

    Function at full functionality

--- 3_Testing/1_Jest/talkToBob.js
+++ 3_Testing/1_Jest/talkToBob.js
@@ -1,8 +1,11 @@
 function talkToBob(info = "") {
   if(info.endsWith("?") && info !== info.toUpperCase()) return "Sure";
-  if(info === info.toUpperCase()) return "Whoa, chill out!";
+  if(info === info.toUpperCase() && !info.endsWith("?") && info.length !== 0) return "Whoa, chill out!";
+  if(info.endsWith("?")) return "Calm down, I know what I'm doing!";
+  if(info === "") return "Fine. Be that way!";
+  else return 'Whatever.'
 };
 
 module.exports = talkToBob;
 
-// console.log(talkToBob("WHAT?"))
+console.log(talkToBob("whatever"))
