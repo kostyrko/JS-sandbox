e5fcca9bd35157783de60ad059a3891884cf03e6
kostyrko <mkostyrko@gmail.com>
2020-11-17 15:06:03 +0100

    Short form with ReactHookForm

---
+++ 4_React/3_react_hook_form/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import ReactHookForm from "./components/ReactHookForm";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-md-6 borer py-3">
+            <ReactHookForm/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// https://react-hook-form.com/get-started#Quickstart
+// https://material-ui.com/
+// https://material-ui.com/getting-started/usage/
+// https://www.youtube.com/watch?v=ptzf4g20--k&t=585s&ab_channel=ArturChmaro
