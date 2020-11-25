51633211454d6397f260f45cf2f14177d44da5f3
kostyrko <mkostyrko@gmail.com>
2020-11-25 08:56:53 +0100

    update content

---
+++ 6_TypeScript/2_Understanding_typescript/app.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+function add(n1, n2) {
+    return n1 + n2;
+}
+const num1 = '1';
+const num2 = 2;
+const res = add(num1, num2);
+console.log(res);
+//# sourceMappingURL=app.js.map
---
+++ 6_TypeScript/2_Understanding_typescript/app.ts
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+function add(n1:number, n2:number) {
+    return n1 + n2
+}
+
+const num1= '1'
+const num2 = 2
+const res = add(num1,num2)
+console.log(res);
