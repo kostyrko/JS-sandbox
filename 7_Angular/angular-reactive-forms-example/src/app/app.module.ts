47894fa40e0461d39b95f011cbce66c7aa59b638
kostyrko <mkostyrko@gmail.com>
2020-12-30 12:03:44 +0100

    Using the FormBuilder service to generate controls

--- 7_Angular/angular-reactive-forms-example/src/app/app.module.ts
+++ 7_Angular/angular-reactive-forms-example/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { ProfileEditorFormBuilderComponent } from './profile-editor-form-builder/profile-editor-form-builder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    ProfileEditorFormBuilderComponent
   ],
   imports: [
     BrowserModule,
---
+++ 7_Angular/angular-reactive-forms-example/src/app/profile-editor-form-builder/profile-editor-form-builder.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+// zaimportowanie klasy FormBuilder
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor-form-builder',
+  templateUrl: './profile-editor-form-builder.component.html',
+  styleUrls: ['./profile-editor-form-builder.component.css']
+})
+export class ProfileEditorFormBuilderComponent  {
+  // stworzenie instancji BudowniczegoFormularzy + metoda group oraz zdefiniowanie właściwości modelu // tu wartości są przechowywane w formie tablic
+  profileForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+  });
+  // wstrzyknięcie serwisu FormBuildera
+  constructor(private fb: FormBuilder) { }
+
+  // onSubmit() przechwytuje dane z formularza
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+  // metoda odpowiadająca za aktualizację wartości formularza wykorzystując patchValue()
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Mike',
+      address: {
+        street: '123 Tatooine'
+      }
+    });
+  }
+
+}
