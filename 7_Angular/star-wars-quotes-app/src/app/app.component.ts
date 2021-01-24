9433e0dc36d743f0ca0090965573a507d15380a4
kostyrko <mkostyrko@gmail.com>
2021-01-24 22:10:42 +0100

    Add voting option

--- 7_Angular/star-wars-quotes-app/src/app/app.component.ts
+++ 7_Angular/star-wars-quotes-app/src/app/app.component.ts
@@ -5,22 +5,23 @@ import { Quotation } from 'src/models/quotations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'star-wars-quotes-app';
-  // tablica wpisów podległa interfejsowi
-  quotes: Quotation[] = QUOTES
-  // pole w komponencie, w którym będą przechowywane dane/wykorzystuje wcześniej zdef. interfejs/ inicjalizacja obiektu z wstępnymi danymi w innym przypadku był by null
-  quotation: Quotation = {author: '', sentence: '', votes: 0};
-  showForm = false
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = { author: '', sentence: '', votes: 0 };
+  showForm = false;
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
-  //metoda,która dodaje quotation na początek listy, następnie resetuje obiekt przechowujący
+
   addQuotation() {
     this.quotes.unshift(this.quotation);
-    this.quotation = {author: '', sentence: '', votes: 0};
+    this.quotation = { author: '', sentence: '', votes: 0 };
   }
 
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
 }
