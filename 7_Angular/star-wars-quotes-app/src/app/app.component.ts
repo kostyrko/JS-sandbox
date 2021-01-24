e7b06cfa3479633d6d6bdcea2ae9803c14792753
kostyrko <mkostyrko@gmail.com>
2021-01-24 21:51:08 +0100

    Adding a quote

--- 7_Angular/star-wars-quotes-app/src/app/app.component.ts
+++ 7_Angular/star-wars-quotes-app/src/app/app.component.ts
@@ -9,13 +9,18 @@ import { Quotation } from 'src/models/quotations';
 })
 export class AppComponent {
   title = 'star-wars-quotes-app';
-
+  // tablica wpisów podległa interfejsowi
   quotes: Quotation[] = QUOTES
-
-  // przechowuje wartość zw. z dodawaniem klasy
+  // pole w komponencie, w którym będą przechowywane dane/wykorzystuje wcześniej zdef. interfejs/ inicjalizacja obiektu z wstępnymi danymi w innym przypadku był by null
+  quotation: Quotation = {author: '', sentence: '', votes: 0};
   showForm = false
-  // metoda zmieniająca wartość przechowywaną w showForm
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
+  //metoda,która dodaje quotation na początek listy, następnie resetuje obiekt przechowujący
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = {author: '', sentence: '', votes: 0};
+  }
+
 }
