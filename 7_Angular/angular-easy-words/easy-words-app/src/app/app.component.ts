ed3dea708eb293e996e86258104b20d5e2d98864
kostyrko <mkostyrko@gmail.com>
2021-04-18 16:18:43 +0200

    Parent (service) + child (dump compon.)

--- 7_Angular/angular-easy-words/easy-words-app/src/app/app.component.ts
+++ 7_Angular/angular-easy-words/easy-words-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WordsService } from './services/words.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'easy-words-app';
+
+  constructor(private wordsService: WordsService) {}
+
+  // bezpośredni dostęp do zawartości serwisu
+  // getter rzeczowników
+  get nouns() {
+    return this.wordsService.getNouns()
+  }
+
+  // getter czasowników
+  get verbs() {
+    return this.wordsService.getVerbs()
+  }
 }
--- 7_Angular/angular-easy-words/easy-words-app/src/app/compontents/answers/answers.component.ts
+++ 7_Angular/angular-easy-words/easy-words-app/src/app/compontents/answers/answers.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WordType } from 'src/app/data/models';
 
 @Component({
   selector: 'app-answers',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./answers.component.css']
 })
 export class AnswersComponent implements OnInit {
+
+  // nazwa listy verbs albo nouns
+  @Input() title: string;
+  // przechowuję listę przypisanych słów
+  @Input() words: WordType[];
 
   constructor() { }
 
