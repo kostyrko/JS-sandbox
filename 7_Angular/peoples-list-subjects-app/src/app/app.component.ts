9062053b92f52680f9692a9a8b82bd45160b6baf
kostyrko <mkostyrko@gmail.com>
2021-04-21 20:24:08 +0200

    Initial state + subscription (data load)

--- 7_Angular/peoples-list-subjects-app/src/app/app.component.ts
+++ 7_Angular/peoples-list-subjects-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Person } from './services/data-base.service';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'peoples-list-subjects-app';
+  hidden = true;
+  person: Person = { firstName: '', lastName: '' };
+
+  constructor(private dataService: DataService) {}
+
+  add(): void {
+    // przekazanie informacji z inputa korzystając z metody serwisu
+    this.dataService.addPerson(this.person)
+  }
 }
