d0e69a3db60b5c2a4181a061c93173f9cc56b66a
kostyrko <mkostyrko@gmail.com>
2021-04-25 19:16:09 +0200

    delete other repos

--- 5_Redux/2_redux-thunk/src/app/droidClasses/duck/types.js
+++
@@ -1,9 +0,0 @@
-// przechowuje wszystkie typy
-
-const ADD_DROID_CLASS = 'ADD_DROID_CLASS'
-const RESET_DROID_CLASS = 'RESET_DROID_CLASS'
-
-export default {
-    ADD_DROID_CLASS,
-    RESET_DROID_CLASS
-}
