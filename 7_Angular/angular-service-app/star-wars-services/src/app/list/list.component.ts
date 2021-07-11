748b1ae3360f1012d55c937e7e6aa8c5fdf143bb
Mikołaj Kostyrko <mkostyrko@gmail.com>
2021-07-11 20:57:14 +0200

    create star-wars-service app

---
+++ 7_Angular/angular-service-app/star-wars-services/src/app/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styles: [
+  ]
+})
+export class ListComponent implements OnInit {
+  @Input() characters: [];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
